1f37e7b5fd313182fe5dcc2f1db50f583817183b
2gaudy <agrant9946@gmail.com>
2022-07-15 21:55:21 -0400

    Created hello world i made ball move

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import sys, pygame
 pygame.init()
 
 size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+speed = [0, 0]
+black = 255, 255, 0
 
 screen = pygame.display.set_mode(size)
 
@@ -13,14 +13,40 @@ ballrect = ball.get_rect()
 
 while 1:
     for event in pygame.event.get():
+        if event.type == 771:
+            print("Key was pressed!")
+            if event.text == "s":
+                speed[1] = 1
+
+            if event.text == "w":
+                speed[1] = -1
+
+            if event.text == "a":
+                speed[0] = -1
+
+            if event.text == "d":
+                speed[0] = 1
+
+        if event.type == 769:
+            if event.unicode == "s":
+                speed[1] = 0
+            if event.unicode == "w":
+                speed[1] = 0
+            if event.unicode == "a":
+                speed[0] = 0
+            if event.unicode == "d":
+                speed[0] = 0
+
+        print(event.type, event)
         if event.type == pygame.QUIT:
             sys.exit()
 
+
     ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    # if ballrect.left < 0 or ballrect.right > width:
+    #     speed[0] = -speed[0]
+    # if ballrect.top < 0 or ballrect.bottom > height:
+    #     speed[1] = -speed[1]
 
     screen.fill(black)
     screen.blit(ball, ballrect)
